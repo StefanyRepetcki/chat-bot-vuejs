f291121e5be17244b350ff2cc0101e1b3a1c7d2e
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-02-24 00:00:26 -0300

    feat(Home): importacao de chat na home

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <Chat />
+  </div>
+</template>
+
+<script>
+import Chat from '@/components/Chat.vue'
+export default {
+  name: 'Home',
+  components: {
+    Chat
+  }
+}
+</script>
